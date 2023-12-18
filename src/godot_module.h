bd2a9c93ef0b09545e2e5a5dee6de37d79654cc3
ilyabelow <ilyabelow.almost.surely@gmail.com>
2023-12-18 19:45:24 +0400

    better macro

--- src/godot_module.h
+++ src/godot_module.h
@@ -7,8 +7,7 @@
 
 // taken from dagor/prog/gameLibs/publicInclude/dasModules/dasModulesCommon.h
 // typeName and das_alias do not seem to be vital but may be required in the future
-
-#define MAKE_TYPE_FACTORY_ALIAS(TYPE, DAS_DECL_TYPE) \
+#define MAKE_TYPE_FACTORY_ALIAS(TYPE, DAS_DECL_TYPE)           \
 namespace das {                                                \
   template <>                                                  \
   struct typeFactory<TYPE>                                     \
@@ -23,10 +22,19 @@ namespace das {                                                \
   };                                                           \
 }
 
+#define STR(X) #X
+#define DEFER_STR(X) STR(X)
+#define DAS_MEMBER_NAME(CLASS, MEMBER) _##CLASS##_##MEMBER
+
+#define BIND_METHOD(CLASS, METHOD)\
+using DAS_MEMBER_NAME(CLASS, METHOD) = DAS_CALL_MEMBER(CLASS::METHOD);\
+das::addExtern<DAS_CALL_METHOD(DAS_MEMBER_NAME(CLASS, METHOD))>(*this, lib, DEFER_STR(DAS_MEMBER_NAME(CLASS, METHOD)), das::SideEffects::modifyExternal, DAS_CALL_MEMBER_CPP(CLASS::METHOD));
+
+
 MAKE_TYPE_FACTORY(Node2DNative, Node2D);
 MAKE_TYPE_FACTORY_ALIAS(Vector2, tFloat2);
 
-struct Node2DAnnotation : das::ManagedStructureAnnotation<Node2D, false> {
+struct Node2DAnnotation : das::ManagedStructureAnnotation<Node2D> {
     Node2DAnnotation(das::ModuleLibrary & ml) : ManagedStructureAnnotation("Node2DNative", ml) {
     }
 };
@@ -40,10 +48,8 @@ public:
         addAnnotation(das::make_smart<Node2DAnnotation>(lib));
         addAlias(das::typeFactory<Vector2>::make(lib));
 
-        using method_Node2D_rotate = DAS_CALL_MEMBER(Node2D::rotate);
-        das::addExtern<DAS_CALL_METHOD(method_Node2D_rotate)>(*this, lib, "_Node2D_rotate", das::SideEffects::modifyExternal, DAS_CALL_MEMBER_CPP(Node2D::rotate));
-        using method_Node2D_translate = DAS_CALL_MEMBER(Node2D::translate);
-        das::addExtern<DAS_CALL_METHOD(method_Node2D_translate)>(*this, lib, "_Node2D_translate", das::SideEffects::modifyExternal, DAS_CALL_MEMBER_CPP(Node2D::translate));
+        BIND_METHOD(Node2D, rotate)
+        BIND_METHOD(Node2D, translate)
 
         options["tool"] = das::Type::tBool;
 
